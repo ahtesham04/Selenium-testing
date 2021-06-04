2e78bcbfd18bf367414eb1a19d5c1d4f91351807
ahtesham04 <75575193+ahtesham04@users.noreply.github.com>
2021-06-04 21:33:26 +0530

    Update EcomResource.java

--- src/main/java/com/ecomerce/webapp/EcomResource.java
+++ src/main/java/com/ecomerce/webapp/EcomResource.java
@@ -9,6 +9,6 @@ public class EcomResource {
 	
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(value = "name", defaultValue="World") String name) {
-		return "Hellow world application, on ec2 by user,  hey" + name;
+		return "Hellow world application, on ec2 by user,  hey mr" + name;
 	}
 }
